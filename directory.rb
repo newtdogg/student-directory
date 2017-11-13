e5079909c72872d2f82f848e81ea9a678e1aa524
Ted Newton <edwardfrancisnewton@live.com>
2017-11-13 22:15:37 +0000

    Tidying up and reorganising code for readibility

--- directory.rb
+++ directory.rb
@@ -1,22 +1,3 @@
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def save_students
-  file= File.open("students.csv", "w")
-    @students.each do |student|
-    student_data = [student [:name], student [:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
 def interactive_menu
   @students = []
   loop do
@@ -42,21 +23,6 @@ def process(selection)
   end
 end
 
-
-def print_menu
-  puts "1. Input the sudents"
-  puts "2. Show the students"
-  puts "3. Save to directory.csv"
-  puts "4. Load from directory.csv"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print(@students)
-  print_footer(@students)
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -70,23 +36,55 @@ def input_students
   @students
 end
 
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
+end
+
+def save_students
+  file= File.open("students.csv", "w")
+    @students.each do |student|
+    student_data = [student [:name], student [:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+
+
+def print_menu
+  puts "1. Input the sudents"
+  puts "2. Show the students"
+  puts "3. Save to directory.csv"
+  puts "4. Load from directory.csv"
+  puts "9. Exit"
+end
+
+
 def print_header
-puts "The students of Villains Academy".center(100)
-puts "-------------".center(100)
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
 def print(names)
-names.each_with_index do |x, index|
-puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)".center(100)
-end
+  names.each_with_index do |x, index|
+    puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)".center(100)
+  end
 end
 
 def print_footer(names)
-  if names.count > 1
     puts "Overall, we have #{names.count} great #{plural(@students)}".center(100)
-  else
-    puts "Overall, we have #{names.count} great #{plural(@students)}".center(100)
-  end
 end
 
 def enter_info
